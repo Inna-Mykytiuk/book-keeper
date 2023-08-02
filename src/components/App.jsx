d23da1fdec9ea06e62661f9db0b1166701502a56
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-08-02 23:12:10 +0300

    search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
+// components/App.js
+import React from 'react';
+import Todo from './Todo/Todo';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Todo />
     </div>
   );
 };
+
+export default App;
---
+++ src/components/redux/reducer.js
@@ -0,0 +1,9 @@
+// reducers/index.js
+import { combineReducers } from 'redux';
+import tasksReducer from './taskReducer';
+
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+});
+
+export default rootReducer;
